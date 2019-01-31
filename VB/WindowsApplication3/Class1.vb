5db1122cc13704b4a4376f7b7a3ddee46596d6b3
DevExpressExampleBot <fakeEmail>
2019-01-31 17:47:19 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication3/Class1.vb
+++ VB/WindowsApplication3/Class1.vb
@@ -1,11 +1,10 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
 Imports System.Threading.Tasks
 
 Namespace WindowsApplication3
-	Friend Class Class1
-	End Class
+    Friend Class Class1
+    End Class
 End Namespace
--- VB/WindowsApplication3/Form1.vb
+++ VB/WindowsApplication3/Form1.vb
@@ -1,20 +1,20 @@
-Imports Microsoft.VisualBasic
-Imports DevExpress.XtraEditors
+﻿Imports DevExpress.XtraEditors
 Imports System
 
 
 Namespace WindowsApplication3
-	Partial Public Class Form1
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits XtraForm
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim helper As New GridHScrollHelper(gridView1)
-			helper.EnableScrollByColumns()
-			gridControl1.DataSource = ItemList.GetList()
-			gridView1.Columns(0).Fixed = DevExpress.XtraGrid.Columns.FixedStyle.Left
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            Dim helper As New GridHScrollHelper(gridView1)
+            helper.EnableScrollByColumns()
+            gridControl1.DataSource = ItemList.GetList()
+            gridView1.Columns(0).Fixed = DevExpress.XtraGrid.Columns.FixedStyle.Left
+        End Sub
+    End Class
 End Namespace
